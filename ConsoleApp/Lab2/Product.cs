e01cdb28dba3ec172fc33851c456af77840456e7
Your Name <you@example.com>
2020-04-19 02:13:14 +0700

    first commit

---
+++ ConsoleApp/Lab2/Product.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.Lab2
+{
+    public class Product
+    {
+        public int iD;
+        public string name;
+        public int price;
+        public int qty;
+        public string image;
+        public string desc;
+        private List<String> gallery = new List<String>();
+        public List<Product> productList = new List<Product>();
+
+        public Product()
+        {
+        }
+
+        public Product(int iD, string name, int price, int qty, string image, string desc)
+        {
+            this.iD = iD;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+        }
+        public void addProduct(Product product)
+        {
+            productList.Add(product);
+        }
+        public void getInfo()
+        {
+            foreach (Product p in productList)
+            {    
+                Console.WriteLine("ID:"+p.iD+" Name:"+p.name+" price:"+p.price+" QTY:"+p.qty+" Image:"+p.image+" Desc:"+p.desc);
+                Console.WriteLine();
+            }
+            
+        }
+
+        public void checkStock()
+        {
+            string name;
+            Console.WriteLine("enter product name to check:");
+            name = Convert.ToString(Console.ReadLine());
+            foreach (Product p in productList)
+            {
+                if (name.Equals(p.name))
+                {
+                    if (p.qty > 0)
+                    {
+                        Console.WriteLine("Con hang");
+                    }
+                    else
+                    {
+                        Console.WriteLine("het hang");
+                    }
+                }
+            }
+        }
+
+        public void addGallery()
+        {
+            if (gallery.Count < 2)
+            {
+                foreach (Product p in productList)
+                {
+                    gallery.Add(p.image);
+                }
+                Console.WriteLine("Them thanh cong vao gallery");
+                Console.WriteLine("Gallery List:");
+                foreach (String s in gallery)
+                {
+                    Console.WriteLine(s);   
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vuot qua so luong them ");
+            }
+        }
+        public void removeGallery()
+        {
+            string removegallery;
+            Console.WriteLine("Nhap ten anh muon xoa:");
+            removegallery = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < gallery.Count; i++)
+            {
+                if (removegallery.Equals(gallery[i]))
+                {
+                    gallery.RemoveAt(i);
+                    Console.WriteLine("Xoa thanh cong");
+                    Console.WriteLine("Gallery List:");
+                    foreach (String s in gallery)
+                    {
+                        Console.WriteLine(s);   
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("khong tim thay anh de xoa");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Lab2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp.Lab2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            int choice,iD, qty,price;
+            string name,image, desc;
+            do
+            {
+                Console.WriteLine("1: Add product:");
+                Console.WriteLine("2: Get Info:");
+                Console.WriteLine("3: Check Stock: ");
+                Console.WriteLine("4: Add gallery");
+                Console.WriteLine("5: Remove gallery");
+                Console.WriteLine("6: Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: 
+                        Console.WriteLine("Enter ID:");
+                        iD = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter name:");
+                        name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter price:");
+                        price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter qty:");
+                        qty = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter image:");
+                        image = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter desc:");
+                        desc = Convert.ToString(Console.ReadLine());
+                        p1.addProduct(new Product(iD,name,price,qty,image,desc));
+                        break;
+                    case 2: 
+                        p1.getInfo();;
+                        break;
+                    case 3: 
+                        p1.checkStock();
+                        break;
+                    case 4: 
+                        p1.addGallery();
+                        break;
+                    case 5:
+                        p1.removeGallery();
+                        break;
+                    case 6: 
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice please choice 1-6!");
+                        break;
+                }
+            } while (choice != 6);
+        }
+    }
+}
